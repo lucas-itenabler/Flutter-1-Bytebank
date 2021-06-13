8a74c023dd1d9b0145b19ba3557b9e0ec310c0b4
Lucas Santana <lsantana.2015@alunos.utfpr.edu.br>
2021-06-13 20:45:27 -0300

    Montando o layout do firmulário

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -17,8 +17,40 @@ class FormularioTransferencia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Criando Transferência'),),
-        body: Text('teste'));
+        appBar: AppBar(
+          title: Text('Criando Transferência'),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                style: TextStyle(
+                    fontSize: 24.0
+                ),
+                decoration: InputDecoration(
+                    labelText: 'Número da conta',
+                    hintText: '0000'
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),   Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                style: TextStyle(
+                    fontSize: 24.0
+                ),
+                decoration: InputDecoration(
+                    icon: Icon(Icons.monetization_on),
+                    labelText: 'Valor',
+                    hintText: '0.00'
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            ElevatedButton(onPressed: () {}, child: Text('Confirmar'))
+          ],
+        ));
   }
 }
 
